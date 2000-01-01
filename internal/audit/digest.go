ddc9fe2cb2b82eacb536bffad357dc50c3c79a22
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for ae033f94-c86b-4fd5-9f83-59e948b30574-bug-5

--- internal/audit/digest.go
+++ internal/audit/digest.go
@@ -6,11 +6,12 @@ import (
 	"encoding/json"
 )
 
-type Digester struct {
-	scratch []byte
-}
+// Digester 计算审计事件与快照的 SHA-256 摘要。
+// 它不持有可变状态，因此同一实例可被多个 goroutine 并发调用：
+// 每次调用都在各自独占的字节切片上计算，结果始终等同于独立计算的 SHA-256。
+type Digester struct{}
 
-func NewDigester() *Digester { return &Digester{scratch: make([]byte, 0, 1024)} }
+func NewDigester() *Digester { return &Digester{} }
 func (d *Digester) EventDigest(previous, caseID string, version uint64, kind string, payload []byte) string {
 	canonical, _ := json.Marshal(struct {
 		Previous string          `json:"previous"`
@@ -19,12 +20,10 @@ func (d *Digester) EventDigest(previous, caseID string, version uint64, kind str
 		Kind     string          `json:"kind"`
 		Payload  json.RawMessage `json:"payload"`
 	}{previous, caseID, version, kind, payload})
-	d.scratch = append(d.scratch[:0], canonical...)
-	sum := sha256.Sum256(d.scratch)
+	sum := sha256.Sum256(canonical)
 	return hex.EncodeToString(sum[:])
 }
 func (d *Digester) SnapshotDigest(snapshot []byte) string {
-	d.scratch = append(d.scratch[:0], snapshot...)
-	sum := sha256.Sum256(d.scratch)
+	sum := sha256.Sum256(snapshot)
 	return hex.EncodeToString(sum[:])
 }
